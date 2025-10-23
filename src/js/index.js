4110afdc9b1e6c2d1bbc94d44942c8c9752c375d
Carlos Renato <carlos_sdossantos@hotmail.com>
2025-10-23 17:37:48 -0300

    feat: adiciona total de seguidores e seguindo e código refatorado

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import {fetchGithubUser} from './githubApi.js';
+import { renderProfile } from './profileView.js';
+
 const inputSearch = document.getElementById("input-search");
 const btnSearch = document.getElementById("btn-search");
 const profileResults = document.querySelector(".profile-results");
@@ -7,37 +10,21 @@ const BASE_URL = "https://api.github.com";
 btnSearch.addEventListener("click", async () => {
     const userName = inputSearch.value;
 
-    if (userName) {
-        profileResults.innerHTML = `<p class="loading">Carregando...</p>`
+    if (!userName) {
+
+        alert("Por favor, digite um nome de usuário do Github.")
+        profileResults.innerHTML = "";
+        return;
+    }
+
+      profileResults.innerHTML = `<p class="loading">Carregando...</p>`
         try {
-
-            const response = await fetch(`${BASE_URL}/users/${userName}`);
-
-            if (!response.ok) {
-                alert("Usuário não encontrado. Por favor, verifique o nome do usuário e tente novamente.");
-                profileResults.innerHTML = "";
-                return;
-            }
-
-            const userData = await response.json();
-            console.log(userData);
-
-            profileResults.innerHTML = `<div class="profile-card">
-                                            <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
-                                            <div class="profile-info">
-                                               <h2>${userData.name}</h2>
-                                               <p>${userData.bio || 'Não possui bio cadastrada 😢.'}</p>
-                                            </div>
-                                        </div>`;
+            const userData = await fetchGithubUser(userName);
+            renderProfile(userData, profileResults);
 
         } catch (error) {
             console.error("Erro ao buscar o perfil do usuário:", error);
             alert("Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.")
             profileResults.innerHTML = ""
         }
-
-    } else {
-        alert("Por favor, digite um nome de usuário");
-    }
-
-})
+});
