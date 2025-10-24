067b04ddec5e1a2614f839e76714744ef46b5c7f
Carlos Renato <carlos_sdossantos@hotmail.com>
2025-10-23 23:03:12 -0300

    feat: adiciona lista com 10 últimos repositórios

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,18 @@
+const BASE_URL = "https://api.github.com";
+const repositoiresQuantity = 10;
+
 export async function fetchGithubUser(userName){
-    const BASE_URL = "https://api.github.com";
     const response = await fetch(`${BASE_URL}/users/${userName}`);
     if(!response.ok) {
         throw new Error('Usuário não encontrado.');
+    }
+    return await response.json();
+}
+
+export async function fetchGithubUserRepos(userName){
+    const response = await fetch(`${BASE_URL}/users/${userName}/repos?per_page=${repositoiresQuantity}&sort=created`);
+    if(!response.ok) {
+        throw new Error('Repositórios não encontrado');
     }
     return await response.json();
 }
